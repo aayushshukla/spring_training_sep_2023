2a86e8aa2f20e205c0cb820b6d2b9397562f23c4
Aayush Shukla <aayush.shukla1012@gmail.com>
2023-11-22 21:00:14 +0530

    Add files via upload

--- restdemoexample/src/main/java/com/example/restdemoexample/controller/BookController.java
+++ restdemoexample/src/main/java/com/example/restdemoexample/controller/BookController.java
@@ -1,9 +1,17 @@
 package com.example.restdemoexample.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,6 +56,57 @@ public class BookController {
 	{
 		return bookRepository.findById(bookid).
 				orElseThrow(()-> new BookNotFoundException(bookid));
+	}
+	
+	@DeleteMapping("/deletebook")
+	//@RequestBody
+	//RequestEntity<T>
+	//@RequestParam
+	public ResponseEntity<BookResponse> deleteBook(RequestEntity<Books> requestEntity)
+	{
+		// get request data
+		org.springframework.http.HttpHeaders headers = requestEntity.getHeaders();
+	    headers.forEach((key,value)->{
+	    	System.err.println(String.format("Header '%s' = %s", 
+	    			key,value.stream().collect(Collectors.joining("|"))));
+	    });
+		Books book =	requestEntity.getBody();
+	    
+	    bookRepository.deleteById(book.getBookid());
+	    BookResponse response = new BookResponse();
+	    response.setStatusCode("200");
+	    response.setStatusMsg("Book is deleted");
+	    return ResponseEntity.status(HttpStatus.OK).body(response);
+	    
+	}
+	@PatchMapping("/updatebookinfo")
+	public ResponseEntity<BookResponse>updateBook(@RequestBody Books book)
+	{
+		BookResponse bookResponse = new BookResponse();
+		Optional<Books>  bookinfo =	bookRepository.findById(book.getBookid());
+		if(bookinfo.isPresent())
+		{
+			bookinfo.get().setBookname(book.getBookname());
+			bookinfo.get().setAuthorname(book.getAuthorname());
+			bookRepository.save(bookinfo.get());
+		}
+		else {
+			bookResponse.setStatusCode("400");
+			bookResponse.setStatusMsg("Given Book Not Found");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(bookResponse);
+		}
+		bookResponse.setStatusCode("200");
+		bookResponse.setStatusMsg("Given Book Information Is Updated");
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(bookResponse);
+	}
+	@GetMapping("/allbooksinfo")
+	public List<Books> getAllBooksDetails()
+	{
+		List<Books> bookList = new ArrayList<Books>();
+		bookRepository.findAll().forEach(book->bookList.add(book));
+		return bookList;
 	}
 	
 	
